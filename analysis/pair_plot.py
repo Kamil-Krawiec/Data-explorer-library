626edc49591277c76bbf02b666b3fee42d6b98ab
Maciej Madry <maciek.madry@o2.pl>
2024-11-12 12:22:12 +0100

    Cleanup, moved all of my functions to analysis

--- analysis/pair_plot.py
+++ analysis/pair_plot.py
@@ -3,7 +3,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 def pair_plot(df: pd.DataFrame, exclude_categorical: bool = True) -> None:
+    """
+    Function that create a pair plot between all of numerical values. If `exclude_categorical` flag is set to `True` then
+    the function also includes categorical values as different hues.
+    Args:
+        df: DataFrame to create a pair plot from
+        exclude_categorical: Flag to exclude categorical values from plot
 
+    """
     if exclude_categorical:
         data = df.copy(True).select_dtypes(include='number')
         sns.pairplot(data)
