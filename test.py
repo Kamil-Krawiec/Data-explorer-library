fb8255bf356a6d96b9ace4615bc5092c4102e5ad
Maciej Sieron <maciek_sieron@wp.pl>
2024-11-24 11:39:44 +0100

    costam

--- test.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-labels = ['A', 'B', 'C']
-sizes = [20, 30, 40]
-
-plt.pie([s/100 for s in sizes], labels=labels, autopct='%1.0f%%', startangle=140, normalize=False)
-plt.axis('equal')
-
-plt.show()
